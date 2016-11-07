6bb6f9282952bd5bcb4aa7ddcad1ba03ea25f4df
PomHaffs <tomashaffenden@icloud.com>
2016-11-07 16:50:55 +1100

    fixed err with MainVC naming

---
+++ WishListApp/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  WishListApp
+//
+//  Created by Tomas-William Haffenden on 7/11/16.
+//  Copyright © 2016 PomHaffs. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    
+    @IBOutlet weak var thumb: UIImageView!    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+}
